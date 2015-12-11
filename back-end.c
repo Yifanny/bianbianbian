6fdbe0b8c55eb0a1a3dba8f968ed2e6797683544
Shadow <featherrain26@gmail.com>
2015-12-11 11:06:38 +0100

    back-end

---
+++ back-end.c
@@ -0,0 +1,100 @@
+/*************************************************************************
+    > File Name: back-end.c
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2015年12月10日 星期四 19时33分42秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "struct.h"
+
+typedef struct price {
+	char* name;
+	float euro;
+}price;
+
+price price_list[] = {
+	{"fromage",3.0},
+	{"jambon-beurre".4.0},
+	{"panini",5.0},
+	{"belge",5.0},
+	{"dieppois",4.5}
+};
+
+char* operators = "sans,avec";
+
+float InOrder(node* tree) {
+	int num;
+	float sum;
+	if(tree) {
+		sum = sum + InOrder(tree->left);
+		if(sizeof(tree->content) == sizeof(int)) {
+			printf("\n  %2d ",tree->content);
+			num = tree->content;
+		}
+		else if(strstr(operators, tree->content)){
+			printf("%s",tree->content);
+		}
+		else if(strcmp(tree->content,"et") == 0) {
+			printf("\n  %2d avec ",num);
+		}
+		else if(strcmp(tree->content,"ni") == 0) {
+			printf("\n  %2d sans ",num);
+		}
+		else if(strcmp(tree->content,"mais") == 0) {
+			printf("\n  %2d ",num);
+		}
+		else {
+			sum = 0.5 * num;
+			printf(" %5s%.2f",tree->content,sum2);
+		}
+		sum = sum + InOrder(tree->right);
+	}
+	return sum;
+}
+
+void facture(commandes order[]){
+	int num = 0;
+	int count = 0;
+	float total = 0.0;
+	while(order[count] != NULL){
+		num = (order[count]->head).left->content;
+		printf("\n%2d %10s",num,order[count]->type);
+		int i;
+		float sum1,sum2;
+		for(i = 0; i < 5; i++){
+			if(!strcmp(price_list[i].name,order[count].type)){
+				sum1 = num * price_list[i].euro;
+			}
+		}
+		total = total + sum1;
+		printf("%.2f",sum1);
+		cur = order.head.right;
+		sum2 = InOrder(cur);
+		total = total + sum2;
+		count = count+1;	
+	}
+	printf("\nTotal:%.2f\n",total);
+}
+
+int main() {
+	union ast_c u1;
+	u1.word = NULL;
+	union ast_c u2;
+	u2.num = 1;
+	node* number;
+	number->left = NULL;
+	number->right = NULL;
+	number->content = u2;
+	node tree = {
+		u1,number,NULL;
+	}
+	commandes orders[1];
+	orders[0] = {
+		"panini",tree
+	}
+	facture(orders);
+	return 0;
+}
