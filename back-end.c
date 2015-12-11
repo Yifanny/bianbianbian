b9bd065918cb9f07c1540e22c2b42d231b0f970b
Shadow <featherrain26@gmail.com>
2015-12-11 11:55:36 +0100

    back-end

--- back-end.c
+++ back-end.c
@@ -8,16 +8,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include "struct.h"
+#include "init.h"
 
-typedef struct price {
+struct price {
 	char* name;
 	float euro;
-}price;
+};
 
-price price_list[] = {
+typedef struct price price;
+
+price price_list[5] = {
 	{"fromage",3.0},
-	{"jambon-beurre".4.0},
+	{"jambon-beurre",4.0},
 	{"panini",5.0},
 	{"belge",5.0},
 	{"dieppois",4.5}
@@ -25,30 +27,30 @@ price price_list[] = {
 
 char* operators = "sans,avec";
 
-float InOrder(node* tree) {
+float InOrder(node_c* tree) {
 	int num;
 	float sum;
 	if(tree) {
 		sum = sum + InOrder(tree->left);
 		if(sizeof(tree->content) == sizeof(int)) {
-			printf("\n  %2d ",tree->content);
-			num = tree->content;
+			printf("\n  %2d ",tree->content.num);
+			num = tree->content.num;
 		}
-		else if(strstr(operators, tree->content)){
-			printf("%s",tree->content);
+		else if(strstr(operators, tree->content.word)){
+			printf("%s",tree->content.word);
 		}
-		else if(strcmp(tree->content,"et") == 0) {
+		else if(strcmp(tree->content.word,"et") == 0) {
 			printf("\n  %2d avec ",num);
 		}
-		else if(strcmp(tree->content,"ni") == 0) {
+		else if(strcmp(tree->content.word,"ni") == 0) {
 			printf("\n  %2d sans ",num);
 		}
-		else if(strcmp(tree->content,"mais") == 0) {
+		else if(strcmp(tree->content.word,"mais") == 0) {
 			printf("\n  %2d ",num);
 		}
 		else {
 			sum = 0.5 * num;
-			printf(" %5s%.2f",tree->content,sum2);
+			printf(" %5s%.2f",tree->content.word,sum);
 		}
 		sum = sum + InOrder(tree->right);
 	}
@@ -59,9 +61,9 @@ void facture(commandes order[]){
 	int num = 0;
 	int count = 0;
 	float total = 0.0;
-	while(order[count] != NULL){
-		num = (order[count]->head).left->content;
-		printf("\n%2d %10s",num,order[count]->type);
+	while(count<sizeof(order)){
+		num = (order[count].head).left->content.num;
+		printf("\n%2d %10s",num,order[count].type);
 		int i;
 		float sum1,sum2;
 		for(i = 0; i < 5; i++){
@@ -71,7 +73,7 @@ void facture(commandes order[]){
 		}
 		total = total + sum1;
 		printf("%.2f",sum1);
-		cur = order.head.right;
+		node_c* cur = order[count].head.right;
 		sum2 = InOrder(cur);
 		total = total + sum2;
 		count = count+1;	
@@ -84,17 +86,16 @@ int main() {
 	u1.word = NULL;
 	union ast_c u2;
 	u2.num = 1;
-	node* number;
+	node_c* number;
 	number->left = NULL;
 	number->right = NULL;
 	number->content = u2;
-	node tree = {
-		u1,number,NULL;
-	}
-	commandes orders[1];
-	orders[0] = {
-		"panini",tree
-	}
+	node_c tree = {
+		u1,number,NULL
+	};
+	commandes orders[1] = {
+		{"panini",tree}
+	};
 	facture(orders);
 	return 0;
 }
