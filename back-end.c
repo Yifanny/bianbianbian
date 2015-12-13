75bc831794d8e9fead41d6f221b00953cb4931e5
Yifanny <zyfjane@live.com>
2015-12-13 16:31:51 +0100

    new version for back end

--- back-end.c
+++ back-end.c
@@ -9,67 +9,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include "init.h"
-
-struct price {
-	char* name;
-	float euro;
-};
-
-typedef struct price price;
-
-price price_list[5] = {
-	{"fromage",3.0},
-	{"jambon-beurre",4.0},
-	{"panini",5.0},
-	{"belge",5.0},
-	{"dieppois",4.5}
-};
-
-typedef struct ingredient {
-	float num;
-	char* name;
-}ingredient;
-
-typedef struct cook {
-	char* name;
-	ingredient material[5];
-}cook;
-
-ingredient cook_fromage[3] = {
-	{1.0,"pain"},
-	{10.0,"beurre"},
-	{2.0,"emmental"}
-};
-
-ingredient cook_jambon[4] = {
-	{1.0,"pain"},
-	{1.0,"jambon"},
-	{10.0,"beurre"},
-	{10.0,"salade"}
-};
-
-ingredient cook_panini[5] = {
-	{1.0,"pain"},
-	{1.0,"jambon"},
-	{2.0,"emmental"},
-	{0.5,"tomato"},
-	{10.0,"salade"}
-};
-
-ingredient cook_belge[5] = {
-	{1.0,"pain"},
-	{1.0,"steak"},
-	{50.0,"frites"},
-	{20.0,"mayonnaise"},
-	{10.0,"salade"}
-};
-
-ingredient cook_dieppois[4] = {
-	{1.0,"pain"},
-	{50.0,"thon"},
-	{20.0,"mayonnaise"},
-	{10.0,"salade"}
-};
+#include "back-end.h"
 
 cook* init(){
 	int i;
@@ -237,5 +177,7 @@ void inventaire(version* ver){
 }
 
 int main() {
-
+	version order[2];
+	kind reqs[];
+	
 }
